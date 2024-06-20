69cd814a439d7fd9b7b1d1f2154b025f58d4c44b
gating654 <168896707+gating654@users.noreply.github.com>
2024-06-20 12:18:52 +0700

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,50 @@
+class Animal:
+    alive = True
+    fed = False
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food):
+        if Flower:
+            alive
+            print(f"{self.name} не стал есть {food.name}")
+
+        else:
+            fed = True
+            print(f"{self.name} съел {food.name}")
+
+class Plant:
+    edible = False
+    def __init__(self, name):
+        self.name = name
+
+
+class Mammal(Animal):
+    a = 0
+class Predator(Animal):
+    a = 0
+
+class Flower(Plant):
+    alive = False
+
+
+class Fruit(Plant):
+    edible = True
+
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
+
